675e18c1ced7007a821410b7a2017c271e8f30e9
Roni Dey <ronidey1819@gmail.com>
2023-10-09 02:35:55 +0530

    created admin-details.html

--- controller/controller.go
+++ controller/controller.go
@@ -2,14 +2,15 @@ package controller
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"log"
-	"time"
 	"strconv"
+	"time"
+
 	model "github.com/PratikforCoding/Syntaxia/models"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
-	"fmt"
-	"errors"
 )
 
 type APIConfig struct {
@@ -20,115 +21,117 @@ func NewAPIConfig(attendeesCol *mongo.Collection) *APIConfig {
 	return &APIConfig{AttendeesCollection: attendeesCol}
 }
 
-func (apiCfg *APIConfig)register(attendee model.Attendee) (model.Attendee, error) {
+func (apiCfg *APIConfig) register(attendee model.Attendee) (model.Attendee, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-    defer cancel()
+	defer cancel()
+
+	// Generate a user ID (e.g., a unique serial number)
 	count, err := apiCfg.AttendeesCollection.CountDocuments(ctx, bson.M{})
+	if err != nil {
+		log.Fatal(err)
+	}
 	countStr := strconv.Itoa(int(count) + 1)
-    if err != nil {
-        log.Fatal(err)
-    }
-	attendee.Taken = "no"
 	attendee.SerialNo = countStr
+
+	// Set other attendee properties
+	attendee.Taken = "no"
+
+	// Insert the attendee into the database
 	inserted, err := apiCfg.AttendeesCollection.InsertOne(context.Background(), attendee)
 	if err != nil {
 		log.Fatal(err)
 	}
 	log.Println("Inserted user id:", inserted.InsertedID)
-	createdAttendee, err := apiCfg.getAttendee(attendee.SerialNo)
-	if err != nil {
-		log.Println(err)
-		return model.Attendee{}, err
-	}
-	return createdAttendee, nil
+
+	// Return the created attendee
+	return attendee, nil
 }
 
-func (apiCfg *APIConfig)getAttendee(serial string) (model.Attendee, error) {
-	filter := bson.M{"serialno":serial}
+func (apiCfg *APIConfig) getAttendee(serial string) (model.Attendee, error) {
+	filter := bson.M{"serialno": serial}
 	var user model.Attendee
 	err := apiCfg.AttendeesCollection.FindOne(context.TODO(), filter).Decode(&user)
 	if err != nil {
 		if err == mongo.ErrNoDocuments {
 			fmt.Println("user not found")
-			return model.Attendee{} , errors.New("user not found")
+			return model.Attendee{}, errors.New("user not found")
 		} else {
 			log.Fatal(err)
 		}
 	}
-	
+
 	return user, nil
 }
 
 func (apiCfg *APIConfig) claimefood(serial string) (model.Attendee, error) {
-    filter := bson.M{"serialno": serial}
-    update := bson.M{"$set": bson.M{"taken": "yes"}}
+	filter := bson.M{"serialno": serial}
+	update := bson.M{"$set": bson.M{"taken": "yes"}}
 
-    updateResult, err := apiCfg.AttendeesCollection.UpdateOne(context.TODO(), filter, update)
-    if err != nil {
-        return model.Attendee{}, err
-    }
+	updateResult, err := apiCfg.AttendeesCollection.UpdateOne(context.TODO(), filter, update)
+	if err != nil {
+		return model.Attendee{}, err
+	}
 
-    if updateResult.ModifiedCount == 0 {
-        return model.Attendee{}, errors.New("no document was updated")
-    }
+	if updateResult.ModifiedCount == 0 {
+		return model.Attendee{}, errors.New("no document was updated")
+	}
 
-    var updatedAttendee model.Attendee
-    err = apiCfg.AttendeesCollection.FindOne(context.TODO(), filter).Decode(&updatedAttendee)
-    if err != nil {
-        return model.Attendee{}, err
-    }
+	var updatedAttendee model.Attendee
+	err = apiCfg.AttendeesCollection.FindOne(context.TODO(), filter).Decode(&updatedAttendee)
+	if err != nil {
+		return model.Attendee{}, err
+	}
 
-    return updatedAttendee, nil
+	return updatedAttendee, nil
 }
 
 func (apiCfg *APIConfig) getAllAttendees() ([]model.Attendee, error) {
-    var attendees []model.Attendee
-    filter := bson.M{}
-    
-    cursor, err := apiCfg.AttendeesCollection.Find(context.TODO(), filter)
-    if err != nil {
-        return nil, err
-    }
-    
-    defer cursor.Close(context.TODO())
-    for cursor.Next(context.TODO()) {
-        var attendee model.Attendee
-        if err := cursor.Decode(&attendee); err != nil {
-            return nil, err
-        }
-        attendees = append(attendees, attendee)
-    }
-    
-    if err := cursor.Err(); err != nil {
-        return nil, err
-    }
-    
-    return attendees, nil
+	var attendees []model.Attendee
+	filter := bson.M{}
+
+	cursor, err := apiCfg.AttendeesCollection.Find(context.TODO(), filter)
+	if err != nil {
+		return nil, err
+	}
+
+	defer cursor.Close(context.TODO())
+	for cursor.Next(context.TODO()) {
+		var attendee model.Attendee
+		if err := cursor.Decode(&attendee); err != nil {
+			return nil, err
+		}
+		attendees = append(attendees, attendee)
+	}
+
+	if err := cursor.Err(); err != nil {
+		return nil, err
+	}
+
+	return attendees, nil
 }
 
 func (apiCfg *APIConfig) getAttendeesByYear(year string) ([]model.Attendee, error) {
-    var attendees []model.Attendee
-    
-    filter := bson.M{"year": year}
-    
-    cursor, err := apiCfg.AttendeesCollection.Find(context.TODO(), filter)
-    if err != nil {
-        return nil, err
-    }
-    
-    defer cursor.Close(context.TODO())
-    for cursor.Next(context.TODO()) {
-        var attendee model.Attendee
-        if err := cursor.Decode(&attendee); err != nil {
-            return nil, err
-        }
-        attendees = append(attendees, attendee)
-    }
-    
-    if err := cursor.Err(); err != nil {
-        return nil, err
-    }
-    
-    return attendees, nil
-}
+	var attendees []model.Attendee
 
+	filter := bson.M{"year": year}
+
+	cursor, err := apiCfg.AttendeesCollection.Find(context.TODO(), filter)
+	if err != nil {
+		return nil, err
+	}
+
+	defer cursor.Close(context.TODO())
+	for cursor.Next(context.TODO()) {
+		var attendee model.Attendee
+		if err := cursor.Decode(&attendee); err != nil {
+			return nil, err
+		}
+		attendees = append(attendees, attendee)
+	}
+
+	if err := cursor.Err(); err != nil {
+		return nil, err
+	}
+
+	return attendees, nil
+}
